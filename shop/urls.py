f0f3c326387ee225cbd7660264ca09973f75e800
vsegda_vinovat <riabko97@gmail.com>
2023-10-06 23:45:46 +0300

    small fixes for new templates location

--- shop/urls.py
+++ shop/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from .views import index,single_course
 
 
+
+app_name ='shop'
 urlpatterns = [
     path('',index, name = 'index'),
     path('<int:course_id>',single_course, name='single_course')
