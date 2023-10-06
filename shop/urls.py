5634ab95595c4177395e9f4153a351f39b043a43
vsegda_vinovat <riabko97@gmail.com>
2023-10-06 23:09:45 +0300

    added links for shop app

--- shop/urls.py
+++ shop/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import index
+from .views import index,single_course
 
 
 urlpatterns = [
     path('',index, name = 'index'),
-    path('<int:course_id>',views.single_course, name='single_course')
+    path('<int:course_id>',single_course, name='single_course')
 
 ]
