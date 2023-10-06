807d13879d68b11368d9693369d41bbcdbb8f1b3
vsegda_vinovat <riabko97@gmail.com>
2023-10-06 20:51:29 +0300

    added some view functions and templates

--- shop/urls.py
+++ shop/urls.py
@@ -4,5 +4,7 @@ from .views import index
 
 
 urlpatterns = [
-    path('',index, name = 'index')
+    path('',index, name = 'index'),
+    path('<int:course_id>',views.single_course, name='single_course')
+
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 
+from .models import Course
 
 def index(request):
+    courses = Course.objects.all()
+    return render(request, 'courses.html',{'courses':courses})
 
-    return HttpResponse('Hello from Shop app')
+
+def single_course(request, course_id):
+    course = Course.objects.get(pk=course_id)
+    return render(request,'single_course.html',{'course':course})
