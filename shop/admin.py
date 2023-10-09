a2550eb9599ff0f9b7e43a9cd4d9117d3eab647b
vsegda_vinovat <riabko97@gmail.com>
2023-10-09 20:24:45 +0300

    sub commit

--- shop/admin.py
+++ shop/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from . import models
 
+
+admin.site.site_header = 'Courses Admin'
+admin.site.site_title = 'My Courses'
+
 admin.site.register(models.Category)
 admin.site.register(models.Course)
 
